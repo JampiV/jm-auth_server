2f63430f83e500c0a291828cc80cfed9b499dd47
Jampier <jampier081@gmail.com>
2024-04-26 17:37:47 -0500

    first commit

---
+++ src/main/java/com/jlunic/jlunic_authorization_server/JlunicAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jlunic.jlunic_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlunicAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JlunicAuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jlunic/jlunic_authorization_server/JlunicAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlunic.jlunic_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlunicAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
